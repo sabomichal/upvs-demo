aaf2c56cc996bf8b179ee51509f9cabcfbc88b66
Michal Sabo <sabo@redbyte.eu>
2017-11-07 18:56:15 +0100

    update na novu verziu IAM, update certifatov a klucov pre DEV prostredie

--- src/main/java/eu/redbyte/upvsdemo/security/UpvsAuthnRequestBuilder.java
+++ src/main/java/eu/redbyte/upvsdemo/security/UpvsAuthnRequestBuilder.java
@@ -18,46 +18,48 @@ import java.util.Arrays;
  */
 public class UpvsAuthnRequestBuilder implements AuthnRequestBuilder {
 
-    private static final String protocolBinding = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST";
-    private static final String nameIDFormat = "urn:oasis:names:tc:SAML:2.0:nameid-format:transient";
-    private static final String passwordProtectedTransportClass = "urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport";
-    private static final String smartCardPkiClass = "urn:oasis:names:tc:SAML:2.0:ac:classes:SmartcardPKI";
+	// UPVS podporuje len POST binding
+	public static final String PROTOCOL_BINDING_POST = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-POST";
+	public static final String PROTOCOL_BINDING_REDIRECT = "urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect";
+	public static final String NAMEID_FORMAT_TRANSIENT = "urn:oasis:names:tc:SAML:2.0:nameid-format:transient";
+	public static final String NAMEID_FORMAT_PERSISTENT = "urn:oasis:names:tc:SAML:2.0:nameid-format:persistent";
 
-    private boolean forceAuthn;
-    private boolean isPassive;
+	private boolean forceAuthn;
+	private boolean isPassive;
+	private AuthnContextComparisonTypeEnumeration authnContextComparisonType;
+	private String protocolBinding;
+	private String nameIDFormat;
 
-    public UpvsAuthnRequestBuilder(boolean forceAuthn, boolean isPassive) {
-        this.forceAuthn = forceAuthn;
-        this.isPassive = isPassive;
-    }
+	public UpvsAuthnRequestBuilder(boolean forceAuthn, boolean isPassive, AuthnContextComparisonTypeEnumeration authnContextComparisonType, String protocolBinding, String nameIDFormat) {
+		this.forceAuthn = forceAuthn;
+		this.isPassive = isPassive;
+		this.authnContextComparisonType = authnContextComparisonType;
+		this.protocolBinding = protocolBinding;
+		this.nameIDFormat = nameIDFormat;
+	}
 
-    @Override
-    public AuthnRequest createAuthnRequest(Message message, String issuerId, String assertionConsumerServiceAddress) throws Exception {
-        Issuer issuer = SamlpRequestComponentBuilder.createIssuer(issuerId);
+	@Override
+	public AuthnRequest createAuthnRequest(Message message, String issuerId, String assertionConsumerServiceAddress) throws Exception {
+		Issuer issuer = SamlpRequestComponentBuilder.createIssuer(issuerId);
+		NameIDPolicy nameIDPolicy = SamlpRequestComponentBuilder.createNameIDPolicy(
+						// pokial nie je nameIDFormat transient, nevratia sa nam ziadne assertion atributy
+						true, nameIDFormat, issuerId);
+		AuthnContextClassRef authnCtxClassRefPwd = SamlpRequestComponentBuilder.createAuthnCtxClassRef(
+						"urn:oasis:names:tc:SAML:2.0:ac:classes:PasswordProtectedTransport");
+		AuthnContextClassRef authnCtxClassRefPki = SamlpRequestComponentBuilder.createAuthnCtxClassRef(
+				"urn:oasis:names:tc:SAML:2.0:ac:classes:SmartcardPKI");
+		RequestedAuthnContext authnCtx = SamlpRequestComponentBuilder.createRequestedAuthnCtxPolicy(authnContextComparisonType,
+				Arrays.asList(authnCtxClassRefPwd, authnCtxClassRefPki), null);
 
-        // pokial nie je nameIDFormat transient, nevratia sa nam ziadne assertion atributy
-        NameIDPolicy nameIDPolicy = SamlpRequestComponentBuilder.createNameIDPolicy(
-                true, nameIDFormat, issuerId
-        );
-
-        AuthnContextClassRef authnCtxClassRefPwd = SamlpRequestComponentBuilder.createAuthnCtxClassRef(passwordProtectedTransportClass);
-        // v produkcii uz je akceptovana len smartcard
-        AuthnContextClassRef authnCtxClassRefPki = SamlpRequestComponentBuilder.createAuthnCtxClassRef(smartCardPkiClass);
-        RequestedAuthnContext authnCtx = SamlpRequestComponentBuilder.createRequestedAuthnCtxPolicy(
-                AuthnContextComparisonTypeEnumeration.MAXIMUM,
-                Arrays.asList(authnCtxClassRefPwd, authnCtxClassRefPki), null
-        );
-
-        return SamlpRequestComponentBuilder.createAuthnRequest(
-                assertionConsumerServiceAddress,
-                forceAuthn,
-                isPassive,
-                // UPVS napriek dokumentacii HTTP-Redirect binding neakceptuje, co nam aj potvrdili
-                protocolBinding,
-                SAMLVersion.VERSION_20,
-                issuer,
-                nameIDPolicy,
-                authnCtx
-        );
-    }
+		return SamlpRequestComponentBuilder.createAuthnRequest(
+				assertionConsumerServiceAddress,
+				forceAuthn,
+				isPassive,
+				protocolBinding,
+				SAMLVersion.VERSION_20,
+				issuer,
+				nameIDPolicy,
+				authnCtx
+		);
+	}
 }
